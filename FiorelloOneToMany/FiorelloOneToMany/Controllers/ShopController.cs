4dd87dc8b808046140015d8529f5cf71c594a3dd
jafarova1 <gultajvj@code.edu.az>
2023-05-13 17:11:16 -0700

    added file

---
+++ FiorelloOneToMany/FiorelloOneToMany/Controllers/ShopController.cs
@@ -0,0 +1,31 @@
+﻿using FiorelloOneToMany.Data;
+using FiorelloOneToMany.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiorelloOneToMany.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ShopController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            IEnumerable<Product> products = await _context.Products.Include(m => m.Image).Where(m => m.SoftDelete).Take(4).ToListAsync();
+            return View(products);
+        }
+        [HttpGet]
+        public async Task<IActionResult> ShowMore(int skip)
+        {
+            IEnumerable<Product> products = await _context.Products.Where(m => m.SoftDelete).Skip(skip).Take(4).ToListAsync();
+            return View(products);
+            
+
+        }
+    }
+}
